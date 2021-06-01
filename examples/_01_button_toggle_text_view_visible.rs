976c3b389d1c31321d47bd82daeab0d7003f1d24
wuaoxiang <os.popen@gmail.com>
2021-06-01 21:07:50 +0800

    add screen record for 01_button_toggle_text_view_visible

--- examples/_01_button_toggle_text_view_visible.rs
+++ examples/_01_button_toggle_text_view_visible.rs
@@ -2,21 +2,16 @@ use gtk::prelude::*;
 fn main() {
     let application = gtk::Application::new(None, Default::default());
     application.connect_activate(|application| {
-        let gtk_builder = gtk::Builder::new();
-        gtk_builder
+        let builder = gtk::Builder::new();
+        builder
             .add_from_string(include_str!("_01_button_toggle_text_view_visible.glade"))
             .unwrap();
-        // like js's document.querySelector or android's findViewById
-        let window = gtk_builder.object::<gtk::Window>("window").unwrap();
-        let button = gtk_builder.object::<gtk::Button>("button").unwrap();
-        let label = gtk_builder.object::<gtk::Label>("label").unwrap();
+        let window = builder.object::<gtk::Window>("window").unwrap(); // like js's document.querySelector or android's findViewById
+        let button = builder.object::<gtk::Button>("button").unwrap();
+        let label = builder.object::<gtk::Label>("label").unwrap();
         // move label to button_on_click_listener or use glib::clone! to share label's reference
         button.connect_clicked(move |_| {
-            if label.get_visible() {
-                label.set_visible(false);
-            } else {
-                label.set_visible(true);
-            }
+            label.set_visible(!label.get_visible());
         });
         window.set_application(Some(application));
         window.show_all();
